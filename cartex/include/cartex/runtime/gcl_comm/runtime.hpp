77acabef4e001960b0cdc0d36797169036fef90a
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2021-03-29 15:32:05 +0200

    all orders enabled for gcl

--- cartex/include/cartex/runtime/gcl_comm/runtime.hpp
+++ cartex/include/cartex/runtime/gcl_comm/runtime.hpp
@@ -12,7 +12,6 @@
 #pragma once
 
 #include <vector>
-//#include <boost/variant.hpp>
 
 #include <cartex/runtime/runtime.hpp>
 #include <gridtools/gcl/GCL.hpp>
@@ -25,38 +24,6 @@ namespace gt = gridtools;
 class runtime::impl
 {
   private:
-    //    template<unsigned int I, unsigned int J, unsigned int K>
-    //    using pt = gt::gcl::halo_exchange_dynamic_ut<gt::layout_map<2, 1, 0>, gt::layout_map<I, J, K>,
-    //        runtime::real_type,
-    //#ifdef __CUDACC__
-    //        gt::gcl::gpu
-    //#else
-    //        gt::gcl::cpu
-    //#endif
-    //        >;
-
-    //template<typename T>
-    //static std::unique_ptr<T> make_pattern_impl(MPI_Comm comm)
-    //{
-    //    return std::make_unique<T>(typename T::grid_type::period_type{true, true, true}, comm);
-    //}
-    //
-    //static pattern_variant_t make_pattern(options_values const & options, MPI_Comm comm)
-    //{
-    //    const auto order = options.get_or("order",  std::string("XYZ"));
-    //    if (options.has("MPICart"))  return pattern_variant_t(make_pattern_impl<pt<0,1,2>>(comm));
-    //    else   if (order == "XYZ")   return pattern_variant_t(make_pattern_impl<pt<2,1,0>>(comm));
-    //    else   if (order == "XZY")   return pattern_variant_t(make_pattern_impl<pt<2,0,1>>(comm));
-    //    else   if (order == "YXZ")   return pattern_variant_t(make_pattern_impl<pt<1,2,0>>(comm));
-    //    else   if (order == "YZX")   return pattern_variant_t(make_pattern_impl<pt<0,2,1>>(comm));
-    //    else   if (order == "ZXY")   return pattern_variant_t(make_pattern_impl<pt<1,0,2>>(comm));
-    //    else /*if (order == "ZYX")*/ return pattern_variant_t(make_pattern_impl<pt<0,1,2>>(comm));
-    //}
-    //
-    //using pattern_variant_t = boost::variant<std::unique_ptr<pt<0, 1, 2>>,
-    //    std::unique_ptr<pt<0, 2, 1>>, std::unique_ptr<pt<1, 0, 2>>, std::unique_ptr<pt<2, 0, 1>>,
-    //    std::unique_ptr<pt<1, 2, 0>>, std::unique_ptr<pt<2, 1, 0>>>;
-
     using pattern_type = gt::gcl::halo_exchange_dynamic_ut<gt::layout_map<2, 1, 0>,
         gt::layout_map<0, 1, 2>, runtime::real_type,
 #ifdef __CUDACC__
@@ -69,14 +36,27 @@ class runtime::impl
     struct mpi_cart_holder
     {
         bool     m_create = false;
+        bool     m_reorder = false;
         MPI_Comm m_comm;
+        MPI_Comm m_reorder_comm;
         mpi_cart_holder(options_values const& options, decomposition const& d)
         {
             if (!options.has("MPICart"))
             {
-                const int periods[3] = {1, 1, 1};
                 m_create = true;
-                MPI_Cart_create(d.mpi_comm(), 3, &d.rank_decomposition()[0], periods, 0, &m_comm);
+                const auto order = options.get_or("order", std::string("XYZ"));
+                if (order != "ZYX")
+                {
+                    m_reorder = true;
+                    const auto c = d.coord(0);
+                    const int  key = c[2] + d.rank_decomposition()[2] *
+                                               (c[1] + d.rank_decomposition()[1] * c[0]);
+                    MPI_Comm_split(d.mpi_comm(), 0, key, &m_reorder_comm);
+                }
+                else
+                    m_reorder_comm = d.mpi_comm();
+                const int periods[3] = {1, 1, 1};
+                MPI_Cart_create(m_reorder_comm, 3, &d.rank_decomposition()[0], periods, 0, &m_comm);
             }
             else
             {
@@ -86,20 +66,25 @@ class runtime::impl
         mpi_cart_holder(const mpi_cart_holder&) = delete;
         mpi_cart_holder(mpi_cart_holder&& other)
         : m_create(std::exchange(other.m_create, false))
+        , m_reorder(std::exchange(other.m_reorder, false))
         , m_comm(other.m_comm)
+        , m_reorder_comm(other.m_reorder_comm)
         {
         }
         ~mpi_cart_holder()
         {
-            if (m_create) MPI_Comm_free(&m_comm);
+            if (m_create)
+            {
+                MPI_Comm_free(&m_comm);
+                if (m_reorder) MPI_Comm_free(&m_reorder_comm);
+            }
         }
     };
 
   private:
-    runtime&        m_base;
-    mpi_cart_holder m_cart;
-    pattern_type    m_pattern;
-    //pattern_variant_t                m_pattern_v;
+    runtime&                         m_base;
+    mpi_cart_holder                  m_cart;
+    pattern_type                     m_pattern;
     std::vector<runtime::real_type*> m_field_ptrs;
 
   public:
--- cartex/src/runtime/gcl_comm/runtime.cpp
+++ cartex/src/runtime/gcl_comm/runtime.cpp
@@ -34,16 +34,6 @@ runtime::check_options(options_values const& opts)
         std::cerr << "GCL backend does not support threading" << std::endl;
         return false;
     }
-    // only MPICart allowed
-    if (!opts.has("MPICart"))
-    {
-        const auto order = opts.get_or("order", std::string("XYZ"));
-        if (order != "ZYX")
-        {
-            std::cerr << "GCL backend only supports MPICart or ZYX order" << std::endl;
-            return false;
-        }
-    }
     return true;
 }
 
@@ -51,7 +41,6 @@ runtime::impl::impl(runtime& base, options_values const& options)
 : m_base(base)
 , m_cart(options, base.m_decomposition)
 , m_pattern(typename pattern_type::grid_type::period_type{true, true, true}, m_cart.m_comm)
-//, m_pattern_v(make_pattern(options, base.m_decomposition.mpi_comm()))
 {
     const auto halo = options.get<int>("halo");
     m_pattern.add_halo<0>(halo, halo, halo, m_base.m_domains[0].domain_ext[0] + halo - 1,
@@ -60,12 +49,6 @@ runtime::impl::impl(runtime& base, options_values const& options)
         m_base.m_domains[0].domain_ext[1] + 2 * halo);
     m_pattern.add_halo<2>(halo, halo, halo, m_base.m_domains[0].domain_ext[2] + halo - 1,
         m_base.m_domains[0].domain_ext[2] + 2 * halo);
-    //        boost::apply_visitor([halo,this](auto& pattern) {
-    //            pattern->template add_halo<0>(halo, halo, halo, m_base.m_domains[0].domain_ext[0]+halo-1, m_base.m_domains[0].domain_ext[0]+2*halo);}, m_pattern_v);
-    //        boost::apply_visitor([halo,this](auto& pattern) {
-    //        pattern->template add_halo<1>(halo, halo, halo, m_base.m_domains[0].domain_ext[1]+halo-1, m_base.m_domains[0].domain_ext[1]+2*halo);}, m_pattern_v);
-    //        boost::apply_visitor([halo,this](auto& pattern) {
-    //        pattern->template add_halo<2>(halo, halo, halo, m_base.m_domains[0].domain_ext[2]+halo-1, m_base.m_domains[0].domain_ext[2]+2*halo);}, m_pattern_v);
 }
 
 void
@@ -73,11 +56,6 @@ runtime::impl::init(int)
 {
     for (auto& f : m_base.m_raw_fields[0]) m_field_ptrs.push_back(f.hd_data());
     m_pattern.setup(m_base.m_num_fields);
-    //        boost::apply_visitor([this](auto& pattern) {
-    //            pattern->setup(m_base.m_num_fields);}, m_pattern_v);
-    //#ifdef __CUDACC__
-    //        cudaStreamSynchronize(0);
-    //#endif
 }
 
 void
@@ -86,13 +64,6 @@ runtime::impl::step(int)
     m_pattern.pack(m_field_ptrs);
     m_pattern.exchange();
     m_pattern.unpack(m_field_ptrs);
-    //boost::apply_visitor(
-    //    [this](auto& pattern) {
-    //        pattern->pack(m_field_ptrs);
-    //        pattern->exchange();
-    //        pattern->unpack(m_field_ptrs);
-    //    },
-    //    m_pattern_v);
 }
 
 } // namespace cartex
