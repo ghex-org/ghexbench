bd1b0b23a8c84d26c70263c7fb7258c71a76c852
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2021-04-07 10:56:17 +0200

    removed comments

--- cartex/include/cartex/common/memory.hpp
+++ cartex/include/cartex/common/memory.hpp
@@ -36,12 +36,7 @@ struct memory
 #ifdef __CUDACC__
     struct cuda_deleter
     {
-        // no delete since this messes up the rma stuff
-        // when doing 2 tests in a row!!!
         void operator()(T* ptr) const { cudaFree(ptr); }
-        //void operator()(T*) const
-        //{ /* do nothing */
-        //}
     };
     std::unique_ptr<T[], cuda_deleter> m_device_memory;
 #endif
