4ed69c582dbf2e2099432bb1eb41ec33806561b5
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2022-08-08 10:07:16 +0200

    paired benchmark

--- p2p/src/benchmark_paired.cpp
+++ p2p/src/benchmark_paired.cpp
@@ -21,7 +21,8 @@ namespace p2p
 options&
 benchmark::add_options(options& opts)
 {
-    opts("numa", "number of numa nodes", "n", {1});
+    opts("node", "number of nodes", "n", {1});
+    opts("numa", "number of numa nodes", "u", {1});
     opts("l3", "number of L3 caches", "l", {1});
     return opts;
 }
@@ -29,32 +30,61 @@ benchmark::add_options(options& opts)
 benchmark::benchmark(int& argc, char**& argv)
 : benchmark_base(argc, argv, (ghexbench::num_cpus() / P2P_NUM_HWTHREADS))
 , m_topo{ghexbench::hw_topo::create(MPI_COMM_WORLD)
-             .nodes({1, 1, 1})
+             .nodes({m_options.get<int>("node"), 1, 1})
              .numas({m_options.get<int>("numa"), 1, 1})
              .l3s({m_options.get<int>("l3"), 1, 1})
-             .cores(
-                 {m_mpi_env.size / (m_options.get<int>("l3") * m_options.get<int>("numa")), 1, 1})}
+             .cores({m_mpi_env.size / (m_options.get<int>("l3") * m_options.get<int>("numa") *
+                                          m_options.get<int>("node")),
+                 1, 1})}
 , m_ctx{m_topo.get_comm(), m_threads > 1}
 {
-    //auto const n_l3s = m_topo.count(HWCART_MD_L3CACHE);
-    //if (n_l3s < 2) abort("at least 2 L3 caches expected", m_ctx.rank() == 0);
-    //if (m_threads > 1 && m_threads % 2 != 0)
-    //    abort("even number of cores per rank expected!", m_ctx.rank() == 0);
-    //if (m_mpi_env.size > 1
+    if (m_ctx.size() == 1)
+        abort("at least 2 ranks expected!", m_ctx.rank() == 0);
+    if (m_ctx.size() % 2 != 0)
+        abort("even numer of ranks expected!", m_ctx.rank() == 0);
+
+    auto const n_nodes = m_topo.count(HWCART_MD_NODE);
     auto const n_numas = m_topo.count(HWCART_MD_NUMA);
     auto const n_l3s = m_topo.count(HWCART_MD_L3CACHE);
     auto const n_cores = m_topo.count(HWCART_MD_CORE);
-    //if (n_numas > 1 && n_numas %2 != 0) 
+
+    if (n_nodes > 1)
+    {
+        if (n_nodes % 2 != 0)
+            abort("even numer of nodes expected!", m_ctx.rank() == 0);
+    }
+    else if (n_numas > 1)
+    {
+        if (n_numas % 2 != 0)
+            abort("even numer of numa nodes expected!", m_ctx.rank() == 0);
+    }
+    else if (n_l3s > 1)
+    {
+        if (n_l3s % 2 != 0)
+            abort("even numer of l3 caches expected!", m_ctx.rank() == 0);
+    }
+    else if (n_cores > 1)
+    {
+        if (n_cores % 2 != 0)
+            abort("even numer of cores expected!", m_ctx.rank() == 0);
+    }
 }
 
 oomph::rank_type
 benchmark::peer_rank()
 {
     auto       c = m_topo.level_grid_coord();
+    auto const n_nodes = m_topo.count(HWCART_MD_NODE);
     auto const n_numas = m_topo.count(HWCART_MD_NUMA);
     auto const n_l3s = m_topo.count(HWCART_MD_L3CACHE);
     auto const n_cores = m_topo.count(HWCART_MD_CORE);
-    if (n_numas > 1)
+    if (n_nodes > 1)
+    {
+        auto const node_coord = c[0][0];
+        auto const peer = ((node_coord + n_nodes) + (((node_coord % 2) == 0) ? 1 : -1)) % n_nodes;
+        c[0][0] = peer;
+    }
+    else if (n_numas > 1)
     {
         auto const numa_coord = c[1][0];
         auto const peer = ((numa_coord + n_numas) + (((numa_coord % 2) == 0) ? 1 : -1)) % n_numas;
@@ -77,3 +107,4 @@ benchmark::peer_rank()
 
 } // namespace p2p
 } // namespace ghexbench
+
